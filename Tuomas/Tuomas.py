db0ffb6d5274c3606d297f19ca63b02eeb25b77e
Tuomavl <tulepi2002@gmail.com>
2022-09-28 15:00:33 +0300

    Tuomas

--- Tuomas/Tuomas.py
+++ Tuomas/Tuomas.py
@@ -7,3 +7,17 @@
 #10 lentokenttää, 5 henkilöä, pelaaja saa suorittaa 7 lentoa, 5 tarinaa jossa muuttujina 1 epäillyn nimi
 #Kaikkilta lentokentiltä ei voi lentää jokaiseen, ja kartta miten tää toimii pysyy aina samana.
 #Kuvan laittaminen kartasta pelin sisälle ?
+
+
+#Paina mitä vain aloittaaksesi (tietty)
+#Pelaaja syöttää käyttäjänimen joka lisätään tietokantaan
+#Randomoidaan vaadittavat muuttujat
+#Peli alkaa (Tarina ja säännöt) tekstit printataan osissa hitaasti
+#Peli antaa epäillyjen sijainnit
+#Peli tulostaa mahdolliset suunnat mihin voi lentää
+#Lentojen määrä vähenee joka kerralla
+#Jos kentällä on henkilö -> juttele henkilölle, jos ei anna uusi mahdollisuus lentää muualle
+#Kun lennot on käyttetty, peli kysyy murhaajaa, pelaaja syöttää tiedon
+#Riippuen tuloksesta, printataan joko voitit pelin tai hävisit ja kerrotaan murhaaja.
+#Voitosta lisätään streak ja lisätään voitot listaan jos häviää aloitetaan streak ja lisätään häviöt listaan
+#Lopussa voit painaa jotain aloittaaksesi uudestaan tai lopetaaksesi (print kiitos että pelasit)
