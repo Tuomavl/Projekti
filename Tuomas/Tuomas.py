c539ca4fc731ffe9a1d1c7b9e457a72ca8ff3b6d
Tuomavl <tulepi2002@gmail.com>
2022-09-28 14:33:25 +0300

    Tuomas

---
+++ Tuomas/Tuomas.py
@@ -0,0 +1,9 @@
+#Paina mitä vain aloittaaksesi
+#Pelaaja syöttää käyttäjännimensä ja peli alkaa
+#Tarina alkaa, antaa epäiltyjen sijainnit, kertoo pelin kulun
+#Pelaaja valitsee mihin lähtee lentämään
+#Jos kentällä on henkilö, alkaa keskustelu, jos ei pelaaja voi valita seuraavan kentän mihin mennä
+
+#10 lentokenttää, 5 henkilöä, pelaaja saa suorittaa 7 lentoa, 5 tarinaa jossa muuttujina 1 epäillyn nimi
+#Kaikkilta lentokentiltä ei voi lentää jokaiseen, ja kartta miten tää toimii pysyy aina samana.
+#Kuvan laittaminen kartasta pelin sisälle ?
