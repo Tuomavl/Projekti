54072a9fdaf958ed6d774d54321d68c7e55cff68
Tuomavl <tulepi2002@gmail.com>
2022-10-06 22:40:32 +0300

    Kokoaminen

--- Tuomas/Kokoaminen.py
+++ Tuomas/Kokoaminen.py
@@ -3,18 +3,21 @@ import mysql.connector
 import random
 import time
 
-#Listat:
 
 #Käytettävien maiden lista:
 maat = ['Unkari','Kroatia','Itävalta','Tsekki','Saksa','Tanska','Alankomaat','Italia','Ranska']
 
+
 #Käytettävien henkilöiden lista:
 henkilo = ['Mary','Luke','Sandra','Tom','Adam']
 
-#
+
+#Epäiltyjen randomisointi
 suspect = [0, 1, 2, 3, 4]
 random.shuffle(suspect)
 
+
+#sanakirja henkilöiden tiedoista:
 henkilot = {}
 
 
@@ -37,7 +40,7 @@ kursori=yhteys.cursor()
 
 
 #Anna käyttäjänimi:
-playerName = input("Anna käyttäjänimesi: ")
+playerName = input('Anna käyttäjänimesi: ')
 
 
 #Uusi käyttäjänimi lisätään tauluun, jos käyttäjänimi on sama tämä ohitetaan:
@@ -47,12 +50,10 @@ except:
     print()
 
 
-#Funktiot:
-
 #Alkuteksti:
 def dialogue(text):
     for i in text:
-        print(i, end="")
+        print(i, end='')
         time.sleep(0.005)
     print()
     time.sleep(len(text) / 100)
@@ -65,15 +66,13 @@ def matkustaminen():
     mones = 0
     kursori.execute("select name from flights, gameCountries where gameCountries.countryID=flights.joinID and flights.countryID='" + str(nykMaa) + "';")
     tulos = kursori.fetchall()
-
     kursori.execute("select name from gameCountries where countryID='" + str(nykMaa) + "';")
     maa = kursori.fetchone()
 
 
 #Henkilöiden tekstit ja epäilyt:
-
     if maa[0] !='Puola' and maat.index(maa[0])<5 and henkilo[maat.index(maa[0])] in henkilot:
-        print(f'Tervetuloa! Nimeni on {henkilo[maat.index(maa[0])]}, mielestäni murhaaja ei ole {henkilot[henkilo[maat.index(maa[0])]]}')
+        print(f'\nTervetuloa! Nimeni on {henkilo[maat.index(maa[0])]}, mielestäni murhaaja ei ole {henkilot[henkilo[maat.index(maa[0])]]}')
         print(henkilot)
     elif maa[0] !='Puola' and maat.index(maa[0])<5:
         moi = henkilo[random.randint(1, 4)]
@@ -95,7 +94,7 @@ def matkustaminen():
     lennot-=1
     kursori.execute("select airportName from gameCountries where countryID='" + str(nykMaa) + "';")
     tulos = kursori.fetchone()
-    dialogue(f'Tervetuloa, olet saapunut lentoasemalle: {tulos[0]}')
+    dialogue(f'\nTervetuloa, olet saapunut lentoasemalle: {tulos[0]}')
 
 
 
@@ -107,9 +106,13 @@ valmis = input('Paina enter-näppäintä, kun olet valmis aloittamaan!')
 if lennot == 7:
     random.shuffle(maat)
     random.shuffle(henkilo)
+
+
+#Murhaajakyselyn lista randomisoituna:
 murhaaja = henkilo.copy()
 random.shuffle(murhaaja)
 murhaaja_index = murhaaja.index(henkilo[0])
+
 
 #Jos pelaaja painaa jotain muuta kun enter, peli kysyy uudestaan. Jos pelaaja painaa enter, peli alkaa:
 while valmis != '':
@@ -117,14 +120,14 @@ while valmis != '':
 
 
 #Alkutarina:
-dialogue("Tervetuloa Puolan Varsovaan!")
-dialogue(f"Olet rikostutkija {playerName}")
-dialogue("Eilen myöhään yöllä Ilkka löydettiin murhattuna Varsovasta.")
-dialogue("Sinun tehtäväsi on selvittää kuka murhasi Ilkan.")
-dialogue("Apulaisrikostutkija on kerännyt sinulle viisi epäiltyä, jotka ovat karanneet eri lentokentille ympäri Eurooppaa. ")
-dialogue("Käy haastattelemassa heitä ja selvitä kuka on murhaaja")
-dialogue("mutta muista sinulla on vain 7 lentolippua eli pystyt lentämään vain seitsemään eri kohteeseen, joten käytä lentolippusi harkiten.")
-dialogue("Onnea matkaan!")
+dialogue('\nTervetuloa Puolan Varsovaan!')
+dialogue(f'Olet rikostutkija {playerName}')
+dialogue('Eilen myöhään yöllä Ilkka löydettiin murhattuna Varsovasta.')
+dialogue('Sinun tehtäväsi on selvittää kuka murhasi Ilkan.')
+dialogue('Apulaisrikostutkija on kerännyt sinulle viisi epäiltyä, jotka ovat karanneet eri lentokentille ympäri Eurooppaa. ')
+dialogue('Käy haastattelemassa heitä ja selvitä kuka on murhaaja')
+dialogue('mutta muista sinulla on vain 7 lentolippua eli pystyt lentämään vain seitsemään eri kohteeseen, joten käytä lentolippusi harkiten.')
+dialogue('Onnea matkaan!')
 
 
 #Tulostetaan lista epäiltyjen sijainneista
@@ -137,7 +140,7 @@ dialogue(f'Henkilön nimi:{henkilo[suspect[4]]} ja maa:{maat[suspect[4]]}')
 
 #Peli kysyy uudelleen enter, edetäkseen:
 while valmis != '':
-    valmis = input("Paina enter-näppäintä, kun olet valmis aloittamaan pelin.")
+    valmis = input('Paina enter-näppäintä, kun olet valmis aloittamaan pelin.')
 
 
 #Jos lentoja on jäljellä, voit jatkaa matkustamista:
@@ -146,20 +149,20 @@ while lennot>0:
 
 
 #Tulostaa listan murhaajaehdokkaista:
-print(f"{murhaaja[murhaaja_index]}")
+print(f'{murhaaja[murhaaja_index]}')
 for x in murhaaja:
     print(f'({moni + 1}): {x}')
     moni += 1
 
 
 #Peli kysyy murhaajaan viittaavaa numeroa:
-arvaus = int(input('Mikä on murhaajan numero? '))
+arvaus = int(input('\nMikä on murhaajan numero? '))
 
 
 #Jos oikein = voitit, jos väärin = hävisit:
 if arvaus-1 == murhaaja_index:
     kursori.execute("UPDATE players SET wins=wins+1, amountPlayed=amountPlayed+1, winStreak=winStreak+1 WHERE playerName='"+playerName+"';")
-    print('Oikein, voitit pelin :)')
+    print('\nOikein, voitit pelin :)')
 else:
     kursori.execute("UPDATE players SET losses=losses+1, amountPlayed=amountPlayed+1, winStreak=0 WHERE playerName='"+playerName+"';")
-    print('Väärin, hävisit pelin :(')
+    print('\nVäärin, hävisit pelin :(')
