0a18c55b78a968d2cd716893badc72a854428ce6
Tuomavl <tulepi2002@gmail.com>
2022-10-27 22:00:54 +0300

    27.10

---
+++ Tuomas/Projekti 2/27.10.py
@@ -0,0 +1,51 @@
+# Kirjautumisjärjestelmä sql ja visuaalista
+# Alkuvalikko(Aloita peli, Leaderboard, Asetukset) visuaalista
+# Peliä pelataan kartalla visuaalinen
+# Saapuessaan maahan vastassa on joku joka kertoo tietoja uudessa maassa (Missä oot, mikä on sää) visuaalista ja pelin toimivuutta
+
+#Visuaalinen suunnittelija (HTML ja CSS) (Kerätään ideoita ja jaetaan)
+#Sql (MySql) (kaikki paitsi cristian opettelee)
+#Pelin toimivuus (Python) (tullaan jakamaan)
+#Pythonin ja verkkosivun kommunikointi (JavaScript)
+
+class Alkuvalikko:
+    # Eri painikkeet
+    pass
+
+class Country:
+    # 15 maata joista yksi aloituspaikka
+    # Propertyt:
+    # Aloitussijainti
+    # Missä on pelaaja
+    # Missä on murha-ase
+    # Missä on epäillyt
+    # sää
+    # Maan nimi
+    # Metodit:
+    pass
+
+class Suspect:
+    # 8 Henkilöä
+    # Propertyt:
+    # Nimi
+    # Kuka on murhaaja
+    # Metodit:
+    # Kerro tarina
+    pass
+
+class Murder_weapon:
+    # 5 Asetta
+    # Propertyt:
+    # Sijainti
+    # Mikä se on
+    # Metodit:
+    pass
+
+class Player:
+    # Propertyt:
+    # pelaajan data(voittodata)
+    # Metodit:
+    # Lentää
+    # Ottaa kiinni
+    # Kysyy tietoa (Tallenna)
+    pass
