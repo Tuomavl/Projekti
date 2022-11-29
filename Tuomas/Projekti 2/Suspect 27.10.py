5c6f4e278801ba21de01ac918e40cb3d6fd7f417
Tuomavl <tulepi2002@gmail.com>
2022-11-29 13:34:20 +0200

    28.11

--- Tuomas/Projekti 2/Suspect 27.10.py
+++ Tuomas/Projekti 2/Suspect 27.10.py
@@ -1,8 +1,26 @@
+import mysql.connector
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='LeptuoMARIA2002',
+    autocommit=True
+)
+kursori = yhteys.cursor()
 class Suspect:
-    # 8 Henkilöä
-    # Propertyt:
-    # Nimi
-    # Kuka on murhaaja
+    def __init__(self,name,status):
+        self.name = name
+        self.status = status
+
+    def tellStory(self,name):
+
+        kursori.execute("SELECT story from suspects where name= '" + name + "';")
+        tarina = kursori.fetchone()
+        print(tarina)
     # Metodit:
     # Kerro tarina
     pass
+
+s1 = Suspect('Mary',0)
+s1.tellStory('Luke')
