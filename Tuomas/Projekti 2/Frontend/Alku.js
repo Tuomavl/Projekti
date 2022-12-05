77d8a90b2503a916f46d7f1d4e625a25e9f49b89
Tuomavl <tulepi2002@gmail.com>
2022-12-05 21:00:27 +0200

    5.12

---
+++ Tuomas/Projekti 2/Frontend/Alku.js
@@ -0,0 +1,13 @@
+'use strict';
+let alku = document.getElementById('alku')
+let button = document.createElement('button')
+alku.appendChild(button)
+button.onclick = function(){
+  popup()
+}
+
+function popup(){
+
+}
+
+
