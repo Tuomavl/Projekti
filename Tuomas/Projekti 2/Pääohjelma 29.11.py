4c14764d398c66d4f6bfb47fdd7deb83091fa81d
Tuomavl <tulepi2002@gmail.com>
2022-12-05 12:45:11 +0200

    5.12

--- Tuomas/Projekti 2/Pääohjelma 29.11.py
+++ Tuomas/Projekti 2/Pääohjelma 29.11.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import random
-
+import json
+import requests
 yhteys = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
@@ -167,6 +168,25 @@ Suspects[7].accuse()
 
 print('\n'+playerLocation)
 
+
+# Fetching cityName and airportName from the database
+kursori.execute("SELECT cityName from gameCountries where name='" + str(playerLocation) + "';")
+cityname = kursori.fetchone()
+city_name = cityname[0]
+
+kursori.execute("SELECT airportName from gameCountries where name='" + str(playerLocation) + "';")
+airportname = kursori.fetchone()
+airport_name = airportname[0]
+
+# weather data taken from openweathermap api with cityname
+apikey = '57d9761bd41e88656771c5c3745a9924'
+url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={apikey}&units=metric'
+response = requests.get(url)
+data = json.loads(response.text)
+temp = data['main']['temp']
+
+# Welcome text:
+print(f'Tervetuloa {airport_name} nimiselle lentokentälle!\nOlet nyt kaupungissa {city_name}. Lämpötila on {temp} celsius astetta.')
 player.flyTo()
 
 #playername = input('Give me a name:')
