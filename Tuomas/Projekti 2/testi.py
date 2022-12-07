14fea2e93f0a95ab1c79317924b1a596beba8bee
Tuomavl <tulepi2002@gmail.com>
2022-12-07 17:23:03 +0200

    7.12

--- Tuomas/Projekti 2/testi.py
+++ Tuomas/Projekti 2/testi.py
@@ -9,13 +9,6 @@ def home():
     print(request.form.get('search-box'))
     return render_template('Kirjaudu.html')
 print(result)
-res = str(result)
-
-
-@app.route('/Tarina', methods=["GET","POST"])
-def show_username():
-
-    return render_template('Tarina.html')
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
