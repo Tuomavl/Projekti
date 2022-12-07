531701597b3889efe3269498084b607ec33c0de4
Tuomavl <tulepi2002@gmail.com>
2022-12-07 18:55:18 +0200

    7.12

--- Tuomas/Projekti 2/testi.py
+++ Tuomas/Projekti 2/testi.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request
+import json
+
+
 app = Flask(__name__)
 
-result = []
 
-@app.route('/', methods=["GET","POST"])
-def home():
-    result.append(request.form)
-    print(request.form.get('search-box'))
-    return render_template('Kirjaudu.html')
-print(result)
+@app.route('/username', methods=["GET","POST"])
+def username():
+    result = print(request.form.get('search-box'))
+    return render_template('Kirjaudu.html'),result
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
