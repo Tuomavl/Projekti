dee1951a918e33b125a9dff28d385e6b58c786be
Crosyuo <cristipu@metropolia.fi>
2022-12-11 19:22:12 +0200

    11.12

--- Tuomas/Projekti 2/newApp.py
+++ Tuomas/Projekti 2/newApp.py
@@ -12,11 +12,11 @@ kursori = yhteys.cursor()
 
 from flask import Flask, request
 from flask_cors import CORS
+from player import Player
 
 
 app = Flask(__name__)
 cors = CORS(app)
-
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/',methods =['GET','POST'])
@@ -31,6 +31,44 @@ def leaderboard():
     json_data = json.dumps(leaderboards)
     return json_data
 
+def toinen_funktio(loc):
+    tulos = loc + "Meow"
+    print(tulos)
+    return tulos
+
+@app.route("/newgame/<player>/<loc>")
+def newgame(player, loc):
+    player = f"{player}666"
+    location = toinen_funktio(loc)
+
+    vastaus = {
+        "username": player,
+        "location": location
+    }
+    return vastaus
+
+@app.route("/kirjaudu/<player>")
+def kirjaudu(player):
+    global player1
+    player1 = Player(player)
+    print(player1)
+    print(player1.username)
+
+    vastaus = {
+        "username": player1.username
+    }
+    return vastaus
+
+@app.route("/tarina")
+def tarina():
+    print("Tarina "+player1.username)
+
+    vastaus = {
+        "username": player1.username
+    }
+    return vastaus
+
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
+
 
